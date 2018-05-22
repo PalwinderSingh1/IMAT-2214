2ec661f427d04880cdcbcf2451aafc5d10197f5a
Harminder <P16158110@my365.dmu.ac.uk>
2018-05-22 13:36:34 +0100

    Made product form

---
+++ GITTest/Harmindert.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace GITTest
+{
+    partial class Harminder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetProducts = new System.Windows.Forms.Button();
+            this.lstboxGetProducts = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGetProducts
+            // 
+            this.btnGetProducts.Location = new System.Drawing.Point(12, 23);
+            this.btnGetProducts.Name = "btnGetProducts";
+            this.btnGetProducts.Size = new System.Drawing.Size(88, 25);
+            this.btnGetProducts.TabIndex = 0;
+            this.btnGetProducts.Text = "Get Products";
+            this.btnGetProducts.UseVisualStyleBackColor = true;
+            this.btnGetProducts.Click += new System.EventHandler(this.btnGetProducts_Click);
+            // 
+            // lstboxGetProducts
+            // 
+            this.lstboxGetProducts.FormattingEnabled = true;
+            this.lstboxGetProducts.Location = new System.Drawing.Point(12, 71);
+            this.lstboxGetProducts.Name = "lstboxGetProducts";
+            this.lstboxGetProducts.Size = new System.Drawing.Size(295, 329);
+            this.lstboxGetProducts.TabIndex = 1;
+            // 
+            // Harminder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 464);
+            this.Controls.Add(this.lstboxGetProducts);
+            this.Controls.Add(this.btnGetProducts);
+            this.Name = "Harminder";
+            this.Text = "Harminder";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetProducts;
+        private System.Windows.Forms.ListBox lstboxGetProducts;
+    }
+}
---
+++ GITTest/Harmindert.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace GITTest
+{
+    public partial class Harminder : Form
+    {
+        public Harminder()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetProducts_Click(object sender, EventArgs e)
+        {
+            //Create a list to store the products
+            List<string> Products = new List<string>();
+            //Clear the listbox of existing contents
+            lstboxGetProducts.Items.Clear();
+
+            //Create the database string for Set 1 and Set 2
+            string connectionStringSet1 = Properties.Settings.Default.Data_set_1ConnectionString;
+
+            using (OleDbConnection connection = new OleDbConnection(connectionStringSet1))
+            {
+                connection.Open();
+                OleDbDataReader reader = null;
+                //Obtain the product category, subcategory and name from Dataset1
+                OleDbCommand getProducts = new OleDbCommand("SELECT [Category], [Sub-Category], [Product Name] from Sheet1'", connection);
+                reader = getProducts.ExecuteReader();
+                while (reader.Read())
+                {
+                    //Add the results to the list, with category, subcategory and product name separated by an underscore
+                    //This allows them to be separated and divided into their respective database columns through the split command later on
+                    Products.Add(reader[0].ToString());
+                    Products.Add(reader[1].ToString());
+                    
+                }
+            }
+
+            //create a new list for the formatted data 
+            List<string> ProductsFormatted = new List<String>();
+
+            foreach (string product in Products)
+            {
+                //split the string on whitespace and remove anything thats blank 
+                var products = product.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                //grab first item (we know this is the date ) and add it to your own list 
+                ProductsFormatted.Add(Products[0]);
+            }
+
+
+            //blind the listbox to the list
+            lstboxGetProducts.DataSource = ProductsFormatted;
+
+                
+            }
+
+        }
+    }

b68be99c6a6f51340a988c08cf72dcec3d71b35b
Palwinder <p16223568@my365.dmu.ac.uk>
2018-05-22 12:00:56 +0100

    changed form name

--- GITTest/Form1.Designer.cs
+++ GITTest/Form1.Designer.cs
@@ -28,48 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnGetDates = new System.Windows.Forms.Button();
+            this.listBoxDates = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Git clone and pull test!";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 77);
+            this.label2.Location = new System.Drawing.Point(102, 148);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.Size = new System.Drawing.Size(18, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = ".";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // btnGetDates
+            // 
+            this.btnGetDates.Location = new System.Drawing.Point(31, 115);
+            this.btnGetDates.Name = "btnGetDates";
+            this.btnGetDates.Size = new System.Drawing.Size(186, 58);
+            this.btnGetDates.TabIndex = 2;
+            this.btnGetDates.Text = "GetDates";
+            this.btnGetDates.UseVisualStyleBackColor = true;
+            this.btnGetDates.Click += new System.EventHandler(this.btnGetDates_Click);
+            // 
+            // listBoxDates
+            // 
+            this.listBoxDates.FormattingEnabled = true;
+            this.listBoxDates.ItemHeight = 25;
+            this.listBoxDates.Location = new System.Drawing.Point(31, 226);
+            this.listBoxDates.Name = "listBoxDates";
+            this.listBoxDates.Size = new System.Drawing.Size(221, 179);
+            this.listBoxDates.TabIndex = 3;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(568, 502);
+            this.Controls.Add(this.listBoxDates);
+            this.Controls.Add(this.btnGetDates);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Palwinder";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGetDates;
+        private System.Windows.Forms.ListBox listBoxDates;
     }
 }
 
--- GITTest/Form1.cs
+++ GITTest/Form1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace GITTest
 {
@@ -25,6 +28,32 @@ namespace GITTest
         private void label2_Click(object sender, EventArgs e)
         {
             //comment here
+        }
+
+        private void btnGetDates_Click(object sender, EventArgs e)
+        {
+            //Create a list to store the dates 
+            List<string> Dates = new List<string>();
+            //clear the listbox of existing contents
+            listBoxDates.Items.Clear();
+
+            //create the database string for Set 1 and Set 2
+            string connectionString = Properties.Settings.Default.Data_set_1ConnectionString;
+           
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbDataReader reader = null;
+                OleDbCommand getDates = new OleDbCommand("SELECT [Order Date], [Ship Date] from Sheet1'", connection);
+
+                reader = getDates.ExecuteReader();
+                while (reader.Read())
+                {
+                    Dates.Add(reader[0].ToString());
+                    Dates.Add(reader[1].ToString());
+                }
+            }
+            listBoxDates.DataSource = Dates;
         }
     }
 }

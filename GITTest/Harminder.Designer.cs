70494002798fbe8da18953cd8630fd63962550a7
Palwinder <P16223568@my365.dmu.ac.uk>
2018-05-23 12:27:30 +0100

    made changes

---
+++ GITTest/Harminder.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace GITTest
+{
+    partial class Harminder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxProducts = new System.Windows.Forms.ListBox();
+            this.btnGetProducts = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxProducts
+            // 
+            this.listBoxProducts.FormattingEnabled = true;
+            this.listBoxProducts.ItemHeight = 25;
+            this.listBoxProducts.Location = new System.Drawing.Point(87, 255);
+            this.listBoxProducts.Name = "listBoxProducts";
+            this.listBoxProducts.Size = new System.Drawing.Size(221, 179);
+            this.listBoxProducts.TabIndex = 6;
+            // 
+            // btnGetProducts
+            // 
+            this.btnGetProducts.Location = new System.Drawing.Point(87, 144);
+            this.btnGetProducts.Name = "btnGetProducts";
+            this.btnGetProducts.Size = new System.Drawing.Size(186, 58);
+            this.btnGetProducts.TabIndex = 5;
+            this.btnGetProducts.Text = "GetProducts";
+            this.btnGetProducts.UseVisualStyleBackColor = true;
+            this.btnGetProducts.Click += new System.EventHandler(this.btnGetProducts_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 177);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ".";
+            // 
+            // Harminder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 637);
+            this.Controls.Add(this.listBoxProducts);
+            this.Controls.Add(this.btnGetProducts);
+            this.Controls.Add(this.label2);
+            this.Name = "Harminder";
+            this.Text = "Harminder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxProducts;
+        private System.Windows.Forms.Button btnGetProducts;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ GITTest/Harminder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GITTest
+{
+    public partial class Harminder : Form
+    {
+        public Harminder()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetProducts_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
